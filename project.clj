17ac13e9277d53c90f6132754adac3656a4f746a
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-05-25 22:54:51 +0200

    Added dev deps

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject conway "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :description "Just conways game of life in clojure"
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dev-dependencies [[org.clojars.autre/lein-vimclojure "1.0.0"]
+                     [lein-vim "1.0.1-SNAPSHOT"]])
