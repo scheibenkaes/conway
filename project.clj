6bb36a22afbb5eb3edb0806d4c6dbf334bf3aa4c
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-05-26 21:44:12 +0200

    Added seesaw as dep

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject conway "1.0.0-SNAPSHOT"
   :description "Just conways game of life in clojure"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [seesaw "1.0.3"]]
   :dev-dependencies [[org.clojars.autre/lein-vimclojure "1.0.0"]
                      [lein-vim "1.0.1-SNAPSHOT"]]
   :main conway.core)
