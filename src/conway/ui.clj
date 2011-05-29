10881df0b62c51a1c1e1e499a719b635e2b83cfa
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-05-29 22:38:13 +0200

    Render loop

--- src/conway/ui.clj
+++ src/conway/ui.clj
@@ -2,13 +2,33 @@
   (:use conway.world)
   (:use [seesaw core color graphics]))
 
+(def running (atom false))
+
+(def current-world (atom #{}))
+
+(def tick-speed 500)
+
+(declare canvas-element)
+
+(defn tick-loop [] 
+  (swap! current-world tick))
+
 (def dead-color (color 200 0 0))
 
 (def living-color (color 0 155 0))
 
+(def start-action 
+  (action :name "Start"
+          :handler (fn [_] 
+                     (invoke-later
+                       (do 
+                         (reset! current-world (generate-rand-world))
+                         (reset! running true)
+                         (tick-loop))))))
+
 (def buttons
   (horizontal-panel
-    :items [(button :text "Start")
+    :items [start-action
             (button :text "Stop")
             "Width" (text :text "10") "Height" (text :text "10")]))
 
@@ -18,13 +38,11 @@
         h (.getHeight c)]
     (doseq [[x y] (whole-world)]
       (draw g
-            (rect (dec x) (dec y) 2 2)
-            (style :foreground (color 120 0 0))))))
+            (rect (dec x) (dec y) 1 1)
+            (style :foreground dead-color)))))
 
 (def canvas-element
-  (let [[w h] *bounds*
-        c (canvas :id :canvas :paint paint-world)] 
-    (vertical-panel :items [c])))
+  (canvas :id :canvas :paint paint-world)) 
 
 (def main-content 
   (border-panel
@@ -32,15 +50,16 @@
     :center canvas-element))
 
 (defn start-ui [] 
-  (do
-    (native!)
-    (invoke-later 
-      (frame 
-        :title "Conway" 
-        :width 640
-        :height 480
-        :pack? false
-        :on-close :exit
-        :visible? true
-        :content main-content))))
+  (let [t (timer (fn [e] (repaint! canvas-element)) :delay tick-speed)]
+    (do
+      (native!)
+      (invoke-later 
+        (frame 
+          :title "Conway" 
+          :width 640
+          :height 480
+          :pack? false
+          :on-close :exit
+          :visible? true
+          :content main-content)))))
 
